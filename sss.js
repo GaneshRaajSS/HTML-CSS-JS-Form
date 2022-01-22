297faf7643ec2f6f2a1289a3baf35a97672dd61b
Ganeshraaj1 <ssganeshraaj@gmail.com>
2022-01-22 21:20:27 +0530

    Form Testing

---
+++ sss.js
@@ -0,0 +1,9 @@
+//let BClear = document.querySelector('button');
+//let inputs = document.querySelectorAll('input');
+
+//BClear.addEventListener('click', () => {
+//    inputs.forEach(input =>  input.value = '');
+//});
+function myFunc(){
+    alert("It will Refresh the Page")
+}
